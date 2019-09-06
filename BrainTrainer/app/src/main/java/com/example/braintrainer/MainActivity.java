1774d1728cdd7af4f05759f5accf081f78ef3cac
naitikudeshi <udeshinaitik@gmail.com>
2019-09-06 17:45:01 +0530

    updated code with timer

--- BrainTrainer/app/src/main/java/com/example/braintrainer/MainActivity.java
+++ BrainTrainer/app/src/main/java/com/example/braintrainer/MainActivity.java
@@ -1,36 +1,43 @@
 package com.example.braintrainer;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 
 import android.os.Bundle;
+import android.os.CountDownTimer;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 import java.util.ArrayList;
 import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
     Button goButton;
+    ConstraintLayout gameLayout;
     ArrayList <Integer> answers = new ArrayList<>();
     int locationOfCorrectAnswer;
     TextView resultTextView;
     TextView scoreTextView;
     TextView sumTextView;
+    TextView timerTextView;
+    Button playButton;
     Button button0;
     Button button1;
     Button button2;
     Button button3;
     int score = 0;
+    int a,b;
     int numberOfQuestions = 0;
     public void chooseAnswer(View view)
     {
-       if ( Integer.toString(locationOfCorrectAnswer).equals(view.getTag().toString()))
+       if (Integer.toString(locationOfCorrectAnswer).equals(view.getTag().toString()))
        {
            resultTextView = findViewById(R.id.resultTextView);
            resultTextView.setText("Correct! :)");
-           resultTextView.setVisibility(View.VISIBLE);
            score++;
        }
        else{
@@ -43,13 +50,36 @@ public class MainActivity extends AppCompatActivity {
     public void start(View view)
     {
         goButton.setVisibility(View.INVISIBLE);
+        playAgain(findViewById(R.id.button0));
+        gameLayout.setVisibility(View.VISIBLE);
     }
+    public void playAgain(View view)
+    {
+        playButton.setVisibility(View.INVISIBLE);
+        score = 0;
+        numberOfQuestions = 0;
+        timerTextView.setText("30s");
+        scoreTextView.setText(Integer.toString(score)+ "/" +Integer.toString(numberOfQuestions));
+        newQuestion();
+        new CountDownTimer(30100, 1000)
+        {
+            @Override
+            public void onTick(long l) {
+                timerTextView.setText(String.valueOf(l/1000)+"s");
+            }
 
+            @Override
+            public void onFinish() {
+                resultTextView.setText("TIME'S UP!");
+                playButton.setVisibility(View.VISIBLE);
+            }
+        }.start();
+    }
     public void newQuestion()
     {
         Random rand = new Random();
-        int a = rand.nextInt(21);
-        int b = rand.nextInt(21);
+        a = rand.nextInt(21);
+        b = rand.nextInt(21);
         sumTextView.setText(Integer.toString(a)+"+"+Integer.toString(b));
         locationOfCorrectAnswer = rand.nextInt(4);
         answers.clear();
@@ -83,6 +113,10 @@ public class MainActivity extends AppCompatActivity {
         button2 = findViewById(R.id.button2);
         button3 = findViewById(R.id.button3);
         goButton = findViewById(R.id.goButton);
-        newQuestion();
+        playButton = findViewById(R.id.playButton);
+        timerTextView = findViewById(R.id.timerTextView);
+        gameLayout = findViewById(R.id.gameLayout);
+        goButton.setVisibility(View.VISIBLE);
+        gameLayout.setVisibility(View.INVISIBLE);
     }
 }
